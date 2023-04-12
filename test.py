b51ecc39052c1a1e6cc17ab89c60428be2892b3e
nikitaopravin <elementary@mail.com>
2023-04-12 21:09:01 +0300

    some_changes

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import streamlit as st
-import pandas as pd
-from pycaret.regression import RegressionExperiment, compare_models
+# import pandas as pd
+# from pycaret.regression import RegressionExperiment, compare_models
 
 
-st.sidebar.file_uploader("Choose a CSV file", key='csv')
+# st.sidebar.file_uploader("Choose a CSV file", key='csv')
 
-if st.session_state.csv != None:
-    st.session_state.df = pd.read_csv(st.session_state.csv)
-    st.session_state.target_name = st.selectbox('Choose target', st.session_state.df.columns)
-    st.session_state.df.drop(st.multiselect('Choose feature to drop', st.session_state.df.columns), axis=1, inplace=True)
-    st.experimental_data_editor(st.session_state.df, height=210)
+# if st.session_state.csv != None:
+#     st.session_state.df = pd.read_csv(st.session_state.csv)
+#     st.session_state.target_name = st.selectbox('Choose target', st.session_state.df.columns)
+#     st.session_state.df.drop(st.multiselect('Choose feature to drop', st.session_state.df.columns), axis=1, inplace=True)
+#     st.experimental_data_editor(st.session_state.df, height=210)
     
-    # st.session_state.model = RegressionExperiment()
-    # st.session_state.model.setup(st.session_state.df, target = st.session_state.target_name, session_id = 123)
-    # best = compare_models()
+#     # st.session_state.model = RegressionExperiment()
+#     # st.session_state.model.setup(st.session_state.df, target = st.session_state.target_name, session_id = 123)
+#     # best = compare_models()
 
-st.session_state
+# st.session_state
+
+st.write('Hello')
