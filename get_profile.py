320227d7f5e2685e0669ec283e18f970bae030b1
Konstantin_G <surkink@gmail.com>
2023-04-17 12:43:02 +0300

    delete old

--- get_profile.py
+++
@@ -1,8 +0,0 @@
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
-import streamlit as st
-
-@st.cache_resource
-def get_profile(data, config):
-    profile_df = ProfileReport(data, config_file=config)
-    return profile_df 
